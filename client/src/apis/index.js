a2a6b24815f74303e7818ad5f60e3f8ab489b65a
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2025-10-04 00:08:51 +0100

    Changing the folder and files structure

--- client/src/apis/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://server:4000/api'; // adjust as needed
-
-export const fetchDashboardData = async () => {
-  const response = await axios.get(`${API_BASE_URL}/dashboard`);
-  return response.data;
-};
--- client/src/helpers/auth.ts
+++
@@ -1,6 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export const checkSession = async (): Promise<boolean> => {
-  const isLogin = await AsyncStorage.getItem('isLogin');
-  return !!isLogin;
-};
